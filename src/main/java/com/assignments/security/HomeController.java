64c8b41c45477d3bbbe92ab0ee7480a7700a4549
addwon <addwon@gmail.com>
2018-02-19 14:04:11 -0500

    Commit 2

--- src/main/java/com/assignments/security/HomeController.java
+++ src/main/java/com/assignments/security/HomeController.java
@@ -1,13 +1,18 @@
 package com.assignments.security;
 
+import com.assignments.security.security.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.security.Principal;
+
 @Controller
 public class HomeController {
     @RequestMapping("/")
-    public String showIndex(Model model){
+    public String showIndex(Model model,Principal p,User user){
+        System.out.println(p.getName());
+        System.out.println(user.getRoles());
         return "index";
     }
     @RequestMapping("/login")
